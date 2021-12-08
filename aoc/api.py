9c200302f0b1fe58f3bc5901eb164c7d419e663a
EL KHADIRI Yassine <yassine.el-khadiri@inria.fr>
2021-12-08 17:53:14 +0100

    add year support in api

--- aoc/api.py
+++ aoc/api.py
@@ -1,9 +1,13 @@
 from bs4 import BeautifulSoup
 import requests as rq
 import requests_cache
+import datetime
 
 
-def input(day):
+def input(day, year=None):
+
+    if not year:
+        year = datetime.datetime.now().year
 
     requests_cache.install_cache("/home/yelkhadi/.aoc/cache")
 
@@ -12,10 +16,13 @@ def input(day):
     with open(session_cookie_location, "r") as session:
         headers = {"Cookie": session.readline()[:-1]}
     
-    return rq.get(f"https://adventofcode.com/2021/day/{day}/input", headers=headers).text
+    return rq.get(f"https://adventofcode.com/{year}/day/{day}/input", headers=headers).text
 
 
-def submit(day, level, answer):
+def submit(day, level, answer, year=None):
+
+    if not year:
+        year = datetime.datetime.now().year
 
     session_cookie_location = "/home/yelkhadi/.aoc/secret"
 
@@ -29,7 +36,7 @@ def submit(day, level, answer):
     headers["Content-Length"] = str(len(content))
 
     res = rq.post(
-        f"https://adventofcode.com/2021/day/{day}/answer",
+        f"https://adventofcode.com/{year}/day/{day}/answer",
         data=content,
         headers=headers
         )
