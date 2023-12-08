8a4929b46675330512f7b2d6aa357eda1f83bc5d
Yassine El Khadiri <yelkhadiri@qblox.com>
2023-12-08 08:30:12 +0100

    Move to pyproject.toml

--- aoc/__init__.py
+++ aoc/__init__.py
@@ -1,3 +1,5 @@
-__version__ = "0.1.0"
+import importlib.metadata
+
+__version__ = importlib.metadata.version("aoc")
 
 from .api import *
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 # Advent of Code
 
-> why didn't I knew about this site until now, this is good stuff!
+> HO HO HO
